ade60582043ce6a3ab391a8d10f4d6b932c8c068
heehee <heeheer_g@naver.com>
2023-04-28 09:03:29 +0900

    도서 대출 조인

--- book/book_list_tb.sql
+++ book/book_list_tb.sql
@@ -6,5 +6,4 @@ FROM
 	book_list_tb blt
     left outer join book_tb bt on(bt.book_id = blt.book_id)
     left outer join book_rental_tb brt on(brt.book_list_id = blt.book_list_id)
-order by
-	bt.book_id
+
